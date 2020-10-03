6ad42b4373da99f8e9ec18199527e74a523bfe34
uzytkownik <danio9996@o2.pl>
2020-10-03 14:27:04 +0200

    Base testy selenium 1

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,20 +2,21 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class BaseTest {
 
     WebDriver driver;
+    WebDriverWait wait;
 
     public void highlightElement(WebDriver driver, WebElement element){
         JavascriptExecutor js = (JavascriptExecutor) driver;
@@ -24,19 +25,22 @@ public class BaseTest {
 
 
     @Before
-public void setUp(){
+    public void setUp(){
     System.setProperty("webdriver.chrome.driver","C:\\chromedriver\\chromedriver.exe");
     driver = new ChromeDriver();
     driver.manage().window().maximize();
+    String devToUrl = "https://dev.to";
+    driver.get(devToUrl);
+    wait = new WebDriverWait(driver, 20);
     }
 
     @Test
     public void firsTest(){
-       String devToUrl = "https://dev.to";
-        driver.get(devToUrl);
+      // String devToUrl = "https://dev.to";
+       // driver.get(devToUrl);
         WebElement week = driver.findElement(By.xpath("//a[@href='/top/week']"));
         week.click();
-        WebDriverWait wait = new WebDriverWait(driver, 20);
+        //WebDriverWait wait = new WebDriverWait(driver, 20);
         wait.until(ExpectedConditions.urlToBe("https://dev.to/top/week"));
         WebElement firstPostOnWeek = driver.findElement(By.cssSelector("h2.crayons-story__title > a"));
         String firstPostOnWeektext = firstPostOnWeek.getText();
@@ -53,11 +57,35 @@ public void setUp(){
         assertEquals("Titles aren't the same", postTitleText, firstPostOnWeektext);
 
 
+    }
+    @Test
+    public void searchBarTesting() {
+        WebElement searchBox = driver.findElement(By.id("nav-search"));
+        highlightElement(driver, searchBox);
+        String searchText = "testing";
+        searchBox.sendKeys(searchText);
+        searchBox.sendKeys(Keys.ENTER);
+        String searchUrl = "https://dev.to/search?q=";
+        String searchingUrlWithText = searchUrl + searchText;
+        wait.until(ExpectedConditions.urlToBe(searchingUrlWithText));
+        List<WebElement> postTilesList = driver.findElements(By.cssSelector("h2.crayons-story__title > a"));
+
+        String textFromFirstElement = postTilesList.get(0).getText();
 
 
+       // for (WebElement element : postTitlesList){
+     //       highlightElement(driver, element );
+        }
+        @Test
+    public void findJavaInNavBar(){
+        WebElement js = driver.findElement(By.cssSelector("div#default-sidebar-element-java > a"));
+        highlightElement(driver, js);
+
+
+        }
 
 
 
     }
-}
+
 
